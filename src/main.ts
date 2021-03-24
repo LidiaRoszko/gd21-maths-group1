17fcec1587fb6fe5d7f80ac693972d72b5adb669
Lidia Roszko <lidia.roszko@tu-dresden.de>
2021-03-25 00:19:52 +0100

    jQuery and SVG.js libraries work

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 console.log('Hello!');
+
+var el = $('#testId');
+el.text("Hello everyone!");
+
+// https://svgjs.com/docs/3.0/getting-started/
+var draw = SVG().addTo('body')
+draw.rect(200, 50).fill('#fff000')
