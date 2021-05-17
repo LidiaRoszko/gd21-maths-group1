e77395e5b4cf3aa495c8e153482bc860333ac1ba
maximilian <maximilian.steiner@iteratec.de>
2021-05-17 15:18:43 +0200

    showing full cart if more than 30

--- abgabe/main.js
+++ abgabe/main.js
@@ -375,7 +375,13 @@ class Game {
 
             while(hayToTransport>10){
                 numberOfWagons++;
-   
+                if(numberOfWagons>2){
+                    if (hayToTransport>10){
+                        hayToTransport= 10;
+
+                    }
+                    break;
+                }
                 hayToTransport-=10;
                 let cargoGroup = this.draw.image('./assets/cart-' + 10 + '.png').height(36).width(100);
                 cargoGroup.attr({ x: train.position.x + 100 - lokOffset*numberOfWagons, y: train.position.y });
